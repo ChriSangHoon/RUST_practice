41c395e50f6738a4929caf0d04b563f9e9595177
Chris <seosanghoon@einsis.com>
2022-08-04 17:21:02 +0900

    Contains digits exercises
    About digit exercises from LeetCode

---
+++ leet_code/exer10.rs
@@ -0,0 +1,22 @@
+//1365. How Many Numbers Are Smaller Than the Current Number
+
+pub fn smaller_numbers_than_current(nums: Vec<i32>) -> Vec<i32> {
+    let mut answer: Vec<i32> = Vec::new();
+    for (i, x) in nums.iter().enumerate() {
+        let mut ans: i32 = 0;
+        for j in nums.iter() {
+            if x > j {
+                ans += 1;
+            }
+        }
+        answer.push(ans)
+    }
+    answer
+}
+
+fn main() {
+    println!(
+        "{:?}",
+        smaller_numbers_than_current([8, 1, 2, 2, 3].to_vec())
+    );
+}
