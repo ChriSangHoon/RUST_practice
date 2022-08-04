bf57d180f490bbafde58e3d038722419edb5dde6
Chris <seosanghoon@einsis.com>
2022-08-04 10:09:24 +0900

    Resolving conflicts and formatting
    Removing the exer1.rs which makes conflicts and formats the files

--- exer5.rs
+++ exer5.rs
@@ -1,8 +1,8 @@
 // 1470. Shuffle the Array
 
 pub fn shuffle(nums: Vec<i32>, n: i32) -> Vec<i32> {
-    let mut array1 : Vec<i32> = Vec::new();
-    for i in 0..n{
+    let mut array1: Vec<i32> = Vec::new();
+    for i in 0..n {
         array1.push(nums[i as usize]);
         array1.push(nums[i as usize + n as usize])
     }
@@ -10,5 +10,5 @@ pub fn shuffle(nums: Vec<i32>, n: i32) -> Vec<i32> {
 }
 
 fn main() {
-    println!("{:?}", shuffle([2,5,1,3,4,7].to_vec(), 3))
+    println!("{:?}", shuffle([2, 5, 1, 3, 4, 7].to_vec(), 3))
 }
