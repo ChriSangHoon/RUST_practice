d051efd648b486bcb5b96509876d8df693bc6812
Chris <seosanghoon@einsis.com>
2022-08-04 11:26:17 +0900

    Review the code and rebase the branch
    Review the code by Min Kim and Rebase the master branch

--- exer1.rs
+++ exer1.rs
@@ -2,6 +2,6 @@ pub fn get_concatenation(nums: Vec<i32>) -> Vec<i32> {
     nums.repeat(2)
 }
 
-fn main(){
-    println!("{:?}", get_concatenation([1,2,3].to_vec()))
+fn main() {
+    println!("{:?}", get_concatenation([1, 2, 3].to_vec()))
 }
--- exer2.rs
+++ exer2.rs
@@ -3,9 +3,8 @@
 pub fn build_array(nums: Vec<i32>) -> Vec<i32> {
     let mut ans: Vec<i32> = Vec::new();
 
-    for (i, _el) in nums.iter().enumerate() {
-        let temp = nums[i];
-        ans.push(nums[temp as usize]);
+    for el in nums.iter() {
+        ans.push(nums[el as usize]);
     }
     ans
 }
