01bf944917299a838e50bb44785ae19204e7a67e
Chris <seosanghoon@einsis.com>
2022-08-04 10:09:24 +0900

    Resolving conflicts and formatting
    Removing the exer1.rs which makes conflicts and formats the files

--- exer3.rs
+++ exer3.rs
@@ -3,8 +3,8 @@
 pub fn running_sum(nums: Vec<i32>) -> Vec<i32> {
     let mut temp = 0;
     let mut ans: Vec<i32> = Vec::new();
-        
-    for x in nums.iter(){
+
+    for x in nums.iter() {
         temp += x;
         ans.push(temp);
     }
@@ -12,5 +12,5 @@ pub fn running_sum(nums: Vec<i32>) -> Vec<i32> {
 }
 
 fn main() {
-    println!("{:?}", running_sum([1,2,3,4,].to_vec()));
+    println!("{:?}", running_sum([1, 2, 3, 4,].to_vec()));
 }
