0fa16c63793c2c4dcba65066055b4b2f9a108993
Chris <seosanghoon@einsis.com>
2022-08-08 13:36:31 +0900

    Minimize the memory usage
    Minimize the memory usage by not using 'for' and not making a new vector

--- leet_code/exer10.rs
+++ leet_code/exer10.rs
@@ -1,17 +1,9 @@
 //1365. How Many Numbers Are Smaller Than the Current Number
 
 pub fn smaller_numbers_than_current(nums: Vec<i32>) -> Vec<i32> {
-    let mut answer: Vec<i32> = Vec::new();
-    for x in nums.iter() {
-        let mut ans: i32 = 0;
-        for j in nums.iter() {
-            if x > j {
-                ans += 1;
-            }
-        }
-        answer.push(ans)
-    }
-    answer
+    nums.iter()
+        .map(|i| nums.iter().filter(|j| j.lt(&i)).count() as i32)
+        .collect()
 }
 
 fn main() {
