1dcf19bc4b9663006baa4d5731acfbdd729a4181
Chris <seosanghoon@einsis.com>
2022-08-09 09:22:07 +0900

    Solve the digit exercises
    Solve the digit exercises without using 'for' iterator

--- leet_code/exer7.rs
+++ leet_code/exer7.rs
@@ -1,17 +1,19 @@
 // 1512. Number of Good Pairs
-
+// Constraints : 1 <= nums.length <= 100, 1 <= nums[i] <= 100
 pub fn num_identical_pairs(nums: Vec<i32>) -> i32 {
-    let mut ans: i32 = 0;
-    for (i, num) in nums.iter().enumerate() {
-        for j in nums[i + 1..].iter() {
-            if num == j {
-                ans += 1
-            }
-        }
-    }
+    let ans = nums
+        .iter()
+        .fold([0i32; 101].to_vec(), |mut acc, &x| {
+            acc[x as usize] += 1;
+            acc
+        })
+        .iter()
+        .filter(|&x| *x > 1)
+        .map(|x| x * (x - 1) / 2)
+        .sum();
     ans
 }
 
 fn main() {
-    println!("{}", num_identical_pairs([1, 1, 1, 1].to_vec()));
+    println!("{}", num_identical_pairs([1, 2, 3, 1, 1, 3].to_vec()));
 }
