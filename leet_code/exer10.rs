8259629f13430c6aa836c333e5c48d0b2034bb10
Chris <seosanghoon@einsis.com>
2022-08-09 09:08:34 +0900

    Use the operator
    Use the operator instead of the '.lt()' method

--- leet_code/exer10.rs
+++ leet_code/exer10.rs
@@ -2,7 +2,7 @@
 
 pub fn smaller_numbers_than_current(nums: Vec<i32>) -> Vec<i32> {
     nums.iter()
-        .map(|i| nums.iter().filter(|j| j.lt(&i)).count() as i32)
+        .map(|i| nums.iter().filter(|j| *j < i).count() as i32)
         .collect()
 }
 
