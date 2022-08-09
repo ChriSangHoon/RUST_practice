aa7706065905a92550fb8f16e3ca947f64c670be
Chris <seosanghoon@einsis.com>
2022-08-09 09:22:07 +0900

    Solve the digit exercises
    Solve the digit exercises without using 'for' iterator

--- leet_code/exer8.rs
+++ leet_code/exer8.rs
@@ -1,22 +1,10 @@
 // 1431. Kids With the Greatest Number of Candies
 
 pub fn kids_with_candies(candies: Vec<i32>, extra_candies: i32) -> Vec<bool> {
-    let mut ans: Vec<bool> = Vec::new();
-    for i in 0..candies.len() {
-        let mut temp_candies = candies.clone();
-        temp_candies[i] += extra_candies;
-        if temp_candies[i] == *temp_candies.iter().max().expect("Error") {
-            ans.push(true);
-        } else {
-            ans.push(false);
-        }
-        // let mut max_candy = temp_candies.iter().max().expect("Error");
-        // match temp_candies[i] {
-        //     max_candy => ans.push(true),
-        //     _ => ans.push(false),
-        // }
-    }
-    ans
+    candies
+        .iter()
+        .map(|i| i + extra_candies >= *candies.iter().max().expect("Error"))
+        .collect()
 }
 
 fn main() {
--- leet_code/exer9.rs
+++ leet_code/exer9.rs
@@ -1,18 +1,12 @@
-//1281. Subtract the Product and Sum of Digits of an Integer
+// 1281. Subtract the Product and Sum of Digits of an Integer
 
 pub fn subtract_product_and_sum(n: i32) -> i32 {
-    let str: String = n.to_string();
-    let mut vec: Vec<char> = Vec::new();
-    for c in str.chars() {
-        vec.push(c);
-    }
-
-    let mut mul: i32 = 1;
-    let mut sum: i32 = 0;
-    for i in 0..vec.len() {
-        mul *= vec[i].to_digit(10).expect("Error") as i32;
-        sum += vec[i].to_digit(10).expect("Error") as i32;
-    }
+    let string = n.to_string();
+    let vec = string
+        .chars()
+        .map(|x| x.to_digit(10).expect("Error") as i32);
+    let mul: i32 = vec.clone().product();
+    let sum: i32 = vec.sum();
     mul - sum
 }
 
